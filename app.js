d26688c0f9f7fb0c5c9663c37726cbf13d205421
Faysal Al-Banna <degreane@gmail.com>
2018-11-11 08:39:18 +0200

    added BootStrap jQuery express-handlebars

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var utils=require('util')
+var expressHbs = require('express-handlebars');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+
+//app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', '.hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
